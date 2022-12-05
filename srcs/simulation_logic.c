9a7863b2bd38c4a2a202fe4def052bfb61d87a3a
benatcastro <benatcastro@gmail.com>
2022-12-05 17:16:40 +0100

    norme

--- srcs/simulation_logic.c
+++ srcs/simulation_logic.c
@@ -6,7 +6,7 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 20:33:52 by bena              #+#    #+#             */
-/*   Updated: 2022/12/05 17:01:45 by becastro         ###   ########.fr       */
+/*   Updated: 2022/12/05 17:16:30 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,15 @@ void	*times_eaten_checker(void *data_ptr)
 	data = data_ptr;
 	philo_counter = data->n_philos;
 	aux = (*data->philo_lst);
-	if(!data->must_eat)
+	if (!data->must_eat)
 		exit(EXIT_FAILURE);
 	while (aux)
 	{
-		// printf("eaten philos: %d p_counter %d ", data->eaten_philos, philo_counter);
-		// printf("id: %d t_eaten: %d e_t %d finis %d\n", aux->id, aux->tv->t_eaten, data->eat_times, aux->finished_eaten);
 		if (aux->tv->t_eaten >= data->eat_times && !aux->finished_eaten)
 		{
-			//  printf("id: %d enters\n", aux->id);
-			// printf("eaten philos: %d p_counter %d ", data->eaten_philos, philo_counter);
 			aux->finished_eaten = true;
 			data->eaten_philos++;
 		}
-		//printf("counter: %d n %d\n", counter, philo_counter);
 		if (data->eaten_philos == philo_counter)
 			break ;
 		aux = aux->next;
